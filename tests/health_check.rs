7c5c13e5c19a6c9fcca3fa4d5bd54feb522af600
Casey <leptone55@gmail.com>
2024-10-20 23:41:17 +0800

    cargo run / test stw

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,23 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::{configuration::get_configuration, routes::run};
+
+// fn spawn_app() -> String {
+//     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+//     let port = listener.local_addr().unwrap().port();
+//     let server = zero2prod::run(listener).expect("Failed to bind address");
+//     let _ = tokio::spawn(server);
+//
+//     format!("http://127.0.0.1:{}", port)
+// }
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
 
     format!("http://127.0.0.1:{}", port)
 }
@@ -28,8 +41,15 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
-    
+
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
@@ -39,7 +59,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .send()
         .await
         .expect("Failed to execute request.");
-
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
