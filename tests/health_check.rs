526c46be3be56a9890bf0c0e13829a6894ea9241
Casey <leptone55@gmail.com>
2024-10-21 08:33:36 -0700

    tests green; before 4.5.12

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,8 +12,29 @@ pub struct TestApp {
 }
 
 static TRACING: LazyLock<()> = LazyLock::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout,
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink,
+        );
+        init_subscriber(subscriber);
+    }
+    // let subscriber = get_subscriber("test".into(), "debug".into());
+    // let subscriber = get_subscriber(
+    //     "zero2prod".into(), "info".into(), std::io::stdout
+    // );
+    // init_subscriber(subscriber);
 });
 
 async fn spawn_app() -> TestApp {
