a5c0eb10d314459c22c8f69b19d5bae932f588a2
Casey <leptone55@gmail.com>
2024-10-30 11:57:16 -0700

    update to book repo

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
-// use std::convert::{TryFrom, TryInto};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
