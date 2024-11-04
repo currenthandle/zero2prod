e29729010fed2fb3942d485e37a4c29231a482fa
Casey <leptone55@gmail.com>
2024-11-03 23:09:03 -0800

    before 7.2.3, all green

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
+use zero2prod::email_client::EmailClient;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -13,10 +14,18 @@ async fn main() -> Result<(), std::io::Error> {
     let connection_pool =
         PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invaild sender email address.");
+
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await?;
+    Ok(())
 }
