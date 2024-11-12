9dedabcd29530117da23c8ce33381a14c24d80d8
Casey <leptone55@gmail.com>
2024-11-12 11:39:25 -0800

    before 7.3.9, all green

--- src/startup.rs
+++ src/startup.rs
@@ -8,10 +8,10 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+use crate::configuration::DatabaseSettings;
 
-pub async fn build(configuration: Settings) -> Result <Server, std::io::Error> {
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.connect_options());
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = get_connection_pool(&configuration.database);
 
     let sender_email = configuration
         .email_client
@@ -25,12 +25,19 @@ pub async fn build(configuration: Settings) -> Result <Server, std::io::Error> {
         timeout,
     );
 
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool, email_client)
 }
 
-pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
@@ -44,4 +51,48 @@ pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) ->
     .listen(listener)?
     .run();
     Ok(server)
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(configuration.connect_options())
+}
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
 }
