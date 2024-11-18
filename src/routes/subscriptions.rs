3ee2e8002d5fe7b6db7bc910c43b11fbfef9857d
Casey <leptone55@gmail.com>
2024-11-17 18:23:26 -0700

    before 8.3, all green

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -60,7 +60,7 @@ pub async fn subscribe(
 
     let subscription_token = generate_subscription_token();
     store_token(&mut transaction, subscriber_id, &subscription_token).await?;
-    
+
     if transaction.commit().await.is_err() {
         return Ok(HttpResponse::InternalServerError().finish());
     }
@@ -166,8 +166,6 @@ pub async fn store_token(
     Ok(())
 }
 
-// We derive `Debug`, easy and painless
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl std::fmt::Display for StoreTokenError {
@@ -181,3 +179,33 @@ impl std::fmt::Display for StoreTokenError {
 }
 
 impl ResponseError for StoreTokenError {}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        // The compiler transparently casts `sqlx::Error` into a `&dyn Error`
+        Some(&self.0)
+    }
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
+
+
+
+
