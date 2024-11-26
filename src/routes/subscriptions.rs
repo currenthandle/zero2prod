e91f06815d38dcae922c4b3a1365677fd2c9d1fd
Casey <leptone55@gmail.com>
2024-11-26 09:45:11 -0600

    before 8.3.2, cargo check: could not convert

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -42,7 +42,7 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
     base_url: web::Data<ApplicationBaseUrl>,
-) -> Result<HttpResponse, actix_web::Error> {
+) -> Result<HttpResponse, SubscribeError> {
     let new_subscriber = match form.0.try_into() {
         Ok(form) => form,
         Err(_) => return Ok(HttpResponse::BadRequest().finish()),
@@ -78,6 +78,19 @@ pub async fn subscribe(
 
     Ok(HttpResponse::Ok().finish())
 }
+
+#[derive(Debug)]
+struct SubscribeError {}
+
+impl std::fmt::Display for SubscribeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Failed to create a new subscriber.")
+    }
+}
+
+impl std::error::Error for SubscribeError {}
+
+impl ResponseError for SubscribeError {}
 
 #[tracing::instrument(
     name = "Send a confirmation email to a new subscriber",
@@ -178,8 +191,6 @@ impl std::fmt::Display for StoreTokenError {
     }
 }
 
-impl ResponseError for StoreTokenError {}
-
 impl std::error::Error for StoreTokenError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         // The compiler transparently casts `sqlx::Error` into a `&dyn Error`
@@ -205,7 +216,3 @@ fn error_chain_fmt(
     }
     Ok(())
 }
-
-
-
-
