230ec6643f471ef8d4845247198e19c2750de2c9
Casey <leptone55@gmail.com>
2024-11-26 14:21:45 -0600

    clean. green

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -43,20 +43,6 @@ impl std::fmt::Debug for SubscribeError {
     }
 }
 
-// impl std::error::Error for SubscribeError {
-//     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-//         match self {
-//             // &str does not implement `Error` - we consider it the root cause
-//             SubscribeError::ValidationError(_) => None,
-//             SubscribeError::StoreTokenError(e) => Some(e),
-//             SubscribeError::SendEmailError(e) => Some(e),
-//             SubscribeError::PoolError(e) => Some(e),
-//             SubscribeError::InsertSubscriberError(e) => Some(e),
-//             SubscribeError::TransactionCommitError(e) => Some(e),
-//         }
-//     }
-// }
-
 impl ResponseError for SubscribeError {
     fn status_code(&self) -> StatusCode {
         match self {
@@ -66,7 +52,7 @@ impl ResponseError for SubscribeError {
     }
 }
 
-#[allow(clippy::async_yields_async)]
+// #[allow(clippy::async_yields_async)]
 #[tracing::instrument(
     name = "Adding a new subscriber.",
     skip(form, pool, email_client, base_url),
@@ -120,7 +106,7 @@ fn generate_subscription_token() -> String {
 
 #[tracing::instrument(
     name = "Send a confirmation email to a new subscriber",
-    skip(email_client, new_subscriber, base_url)
+    skip(email_client, new_subscriber, base_url, subscription_token)
 )]
 pub async fn send_confirmation_email(
     email_client: &EmailClient,
@@ -188,69 +174,8 @@ pub async fn store_token(
     transaction.execute(query).await.map_err(StoreTokenError)?;
     Ok(())
 }
-// impl std::fmt::Display for SubscribeError {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             SubscribeError::ValidationError(e) => write!(f, "{}", e),
-//             // SubscribeError::DatabaseError(_) => write!(f, "???"),
-//             SubscribeError::StoreTokenError(_) => write!(
-//                 f,
-//                 "Failed to store the confirmation token for a new subscriber.",
-//             ),
-//             SubscribeError::SendEmailError(_) => {
-//                 write!(f, "Failed to send a confirmation email.")
-//             }
-//             SubscribeError::PoolError(_) => {
-//                 write!(f, "Failed to acquire a Postgres connection from the pool")
-//             }
-//             SubscribeError::InsertSubscriberError(_) => {
-//                 write!(f, "Failed to insert new subscriber in the database.")
-//             }
-//             SubscribeError::TransactionCommitError(_) => {
-//                 write!(
-//                     f,
-//                     "Failed to commit SQL transaction to store a new subscriber."
-//                 )
-//             }
-//         }
-//     }
-// }
-
-// impl From<reqwest::Error> for SubscribeError {
-//     fn from(e: reqwest::Error) -> Self {
-//         Self::SendEmailError(e)
-//     }
-// }
-
-// impl From<sqlx::Error> for SubscribeError {
-//     fn from(e: sqlx::Error) -> Self {
-//         Self::DatabaseError(e)
-//     }
-// }
-
-// impl From<StoreTokenError> for SubscribeError {
-//     fn from(e: StoreTokenError) -> Self {
-//         Self::StoreTokenError(e)
-//     }
-// }
-
-impl From<String> for SubscribeError {
-    fn from(e: String) -> Self {
-        Self::ValidationError(e)
-    }
-}
 
 pub struct StoreTokenError(sqlx::Error);
-
-impl std::fmt::Display for StoreTokenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#"A database error was encountered while
-            trying to store a subscription token."#
-        )
-    }
-}
 
 impl std::error::Error for StoreTokenError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
@@ -262,6 +187,16 @@ impl std::error::Error for StoreTokenError {
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
+    }
+}
+
+impl std::fmt::Display for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            r#"A database error was encountered while
+            trying to store a subscription token."#
+        )
     }
 }
 
